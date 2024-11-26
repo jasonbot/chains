c5c37bfdffc1ebe0b2c398d8a4b1db53f67c052c
Jason Scheirer <jason.scheirer@gmail.com>
2024-11-26 12:21:59 -0800

    Nil checks, more of them

--- core.go
+++ core.go
@@ -6,6 +6,10 @@ import (
 
 func Each[T any](in []T) func(func(T) bool) {
 	return func(yield func(T) bool) {
+		if in == nil {
+			return
+		}
+
 		for _, v := range in {
 			if !yield(v) {
 				return
@@ -16,6 +20,10 @@ func Each[T any](in []T) func(func(T) bool) {
 
 func Map[T, V any](mapFunc func(T) V, input iter.Seq[T]) func(func(V) bool) {
 	return func(yield func(V) bool) {
+		if mapFunc == nil || input == nil {
+			return
+		}
+
 		for v := range input {
 			if !yield(mapFunc(v)) {
 				return
@@ -25,6 +33,11 @@ func Map[T, V any](mapFunc func(T) V, input iter.Seq[T]) func(func(V) bool) {
 }
 
 func ReduceWithZero[T, V any](collectFunc func(V, T) V, zeroValue V, input iter.Seq[T]) V {
+	if collectFunc == nil || input == nil {
+		var zeroValue V
+		return zeroValue
+	}
+
 	for v := range input {
 		zeroValue = collectFunc(zeroValue, v)
 	}
@@ -39,6 +52,10 @@ func Reduce[T any](collectFunc func(T, T) T, input iter.Seq[T]) T {
 
 func Filter[T any](filterFunc func(T) bool, input iter.Seq[T]) func(func(T) bool) {
 	return func(yield func(T) bool) {
+		if filterFunc == nil || input == nil {
+			return
+		}
+
 		for v := range input {
 			if filterFunc(v) {
 				if !yield(v) {
@@ -49,10 +66,20 @@ func Filter[T any](filterFunc func(T) bool, input iter.Seq[T]) func(func(T) bool
 	}
 }
 
-func Tap[T any](vistor func(T), input iter.Seq[T]) func(func(T) bool) {
+func Tap[T any](visitor func(T), input iter.Seq[T]) func(func(T) bool) {
 	return func(yield func(T) bool) {
+		if visitor == nil {
+			for v := range input {
+				if !yield(v) {
+					return
+				}
+			}
+
+			return
+		}
+
 		for v := range input {
-			vistor(v)
+			visitor(v)
 			if !yield(v) {
 				return
 			}
